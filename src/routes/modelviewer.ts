1550969bded046cc0d1cd4716416717e83a7e7fb
Kbro1989 <149967879+Kbro1989@users.noreply.github.com>
2025-10-26 05:55:04 -0500

    Update modelviewer.ts

--- src/routes/modelviewer.ts
+++ src/routes/modelviewer.ts
@@ -1,4 +1,17 @@
-export async function handleModelViewer(req: Request, env: any) {
-  const html = await env.ASSETS.fetch("/webviewer.html");
-  return new Response(html.body, { headers: { "Content-Type": "text/html" } });
+export async function handleRequest(req: Request, env: any) {
+  // Serve webviewer page
+  const html = await env.ASSETS.get('webviewer.js');
+  const body = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>ElderScape Model Viewer</title>
+      <script src="/webviewer.js" defer></script>
+      <link rel="stylesheet" href="/index.css"/>
+    </head>
+    <body>
+      <div id="rsmv_app"></div>
+    </body>
+  </html>`;
+  return new Response(body, { headers: { "Content-Type": "text/html" } });
 }
